fd8e79eefdd6b3a3adecff8d031c547fd6eec13c
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2022-06-15 20:09:49 -0300

    DONE: something work

--- index.js
+++ index.js
@@ -1,123 +1,131 @@
+const TAMANHO = 50; // Tamanho das entidades do jogo
+const VELOCIDADE = 5; // Velocidade de deslocamento
+
 window.onload = criarSound();
 var quadro = document.getElementById("app");
 var lapis = quadro.getContext("2d");
-document.addEventListener("keydown", setDirection)
+document.addEventListener("keydown", setDirection);
 var player1 = {
-    size: 10 / 2,
-    posicaoX: 5,
-    posicaoY: 5,
-    cor: "#000000"
+  size: TAMANHO / 2,
+  posicaoX: TAMANHO,
+  posicaoY: TAMANHO,
+  cor: "#FF0000",
+  direcao: 0
 }
 var player2 = {
-    size: 10,
-    posicaoX: quadro.width - 10,
-    posicaoY: quadro.height - 10,
-    cor: "#000000"
+  size: TAMANHO,
+  posicaoX: quadro.width - TAMANHO,
+  posicaoY: quadro.height - TAMANHO,
+  cor: "#0000FF",
+  direcao: 0
 }
 desenhar();
 
 function criarSound() {
-    let som = document.createElement("audio");
-    som.src = "./asserts/bulletimpact.mp3";
-    som.setAttribute("id", "som")
-    som.setAttribute("preload", "auto");
-    som.setAttribute("controls", "none");
-    som.style.display = "none";
-    document.body.appendChild(som);
+  let som = document.createElement("audio");
+  som.src = "./asserts/bulletimpact.mp3";
+  som.setAttribute("id", "som")
+  som.setAttribute("preload", "auto");
+  som.setAttribute("controls", "none");
+  som.style.display = "none";
+  document.body.appendChild(som);
 }
 
 function sound() {
-    let som = document.getElementById("som");
-    som.play();
+  let som = document.getElementById("som");
+  som.play();
 }
 
 function setDirection(event) {
-    switch (event.code) {
-        // Teclas para o player1
-        case "KeyA":
-            player1.posicaoX -= 2
-            break
-        case "KeyD":
-            player1.posicaoX += 2
-            break
-        case "KeyS":
-            player1.posicaoY += 2
-            break
-        case "KeyW":
-            player1.posicaoY -= 2
-            break
-        case "ShiftLeft":
-            player1.size += 1
-            break
-        case "ControlLeft":
-            player1.size -= 1
-            break
-            // Teclas para o player2
-        case "ArrowLeft":
-            player2.posicaoX -= 2
-            break
-        case "ArrowRight":
-            player2.posicaoX += 2
-            break
-        case "ArrowDown":
-            player2.posicaoY += 2
-            break
-        case "ArrowUp":
-            player2.posicaoY -= 2
-            break
-        case "ShiftRight":
-            player2.size += 1
-            break
-        case "ControlRight":
-            player2.size -= 1
-            break
-    }
-    requestAnimationFrame(desenhar);
-    if (detectColision() < 1300) {
-        sound();
-    }
+  switch (event.code) {
+    // Teclas para o player1
+    case "KeyA":
+      player1.posicaoX -= VELOCIDADE;
+      break
+    case "KeyD":
+      player1.posicaoX += VELOCIDADE;
+      break
+    case "KeyS":
+      player1.posicaoY += VELOCIDADE;
+      break
+    case "KeyW":
+      player1.posicaoY -= VELOCIDADE;
+      break
+    case "ShiftLeft":
+      player1.size += VELOCIDADE;
+      break
+    case "ControlLeft":
+      player1.size -= VELOCIDADE;
+      break
+      // Teclas para o player2
+    case "ArrowLeft":
+      player2.posicaoX -= VELOCIDADE;
+      break
+    case "ArrowRight":
+      player2.posicaoX += VELOCIDADE;
+      break
+    case "ArrowDown":
+      player2.posicaoY += VELOCIDADE;
+      break
+    case "ArrowUp":
+      player2.posicaoY -= VELOCIDADE;
+      break
+    case "ShiftRight":
+      player2.size += VELOCIDADE;
+      break
+    case "ControlRight":
+      player2.size -= VELOCIDADE;
+      break
+  }
+  requestAnimationFrame(desenhar);
+  if (detectColision() == 1) {
+    sound();
+  }
 }
 
 function detectColision() {
-    lat1 = player1.posicaoX
-    lat2 = player2.posicaoX
-    lon1 = player1.posicaoY
-    lon2 = player2.posicaoY
-
-    rad = function(x) { return x * Math.PI / 180; }
-
-    //Raio da Terra no km (WGS84)
-    R = 6378.137;
-    // Distância latitudinal
-    dLat = rad(lat2 - lat1);
-    // Distância longitudinal
-    dLong = rad(lon2 - lon1);
-
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(rad(lat1)) * Math.cos(rad(lat2)) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
-
-    return d.toFixed(3);
+  lat1 = player1.posicaoX
+  lat2 = player2.posicaoX
+  lon1 = player1.posicaoY
+  lon2 = player2.posicaoY
+  latc = false // colisão de latitude
+  lonc = false // colisão de longitude
+  if (lat1 > lat2) {
+    if ((lat1 - lat2) <= TAMANHO) latc = true;
+  } else {
+    if ((lat2 - lat1) <= TAMANHO) latc = true;
+  }
+  if (lon1 > lon2) {
+    if ((lon1 - lon2) <= TAMANHO) lonc = true;
+  } else {
+    if ((lon2 - lon1) <= TAMANHO) lonc = true;
+  }
+  return (latc & lonc);
 }
 
 function desenhar() {
-    // Limpando a tela para novo desenho
-    lapis.clearRect(0, 0, quadro.width, quadro.height);
-    // Desenhando o player1
-    player1.cor = 'rgb(' + (Math.floor(Math.random() * (255 - 0)) + 0) + ',' + (Math.floor(Math.random() * (255 - 0)) + 0) + ',0)';
-    lapis.beginPath();
-    lapis.strokeStyle = player1.cor;
-    lapis.arc(player1.posicaoX, player1.posicaoY, player1.size, 0, 2 * Math.PI);
-    lapis.stroke();
-    // Desenhando o player2
-    player2.cor = 'rgb(' + (Math.floor(Math.random() * (255 - 0)) + 0) + ',' + (Math.floor(Math.random() * (255 - 0)) + 0) + ',0)';
-    lapis.beginPath();
-    lapis.strokeStyle = player2.cor;
-    lapis.rect(player2.posicaoX, player2.posicaoY, player2.size, player2.size);
-    lapis.stroke();
-    // Desenhando o debug de posicionamentos
-    lapis.font = "8px Arial";
-    lapis.fillStyle = "black";
-    lapis.textAlign = "center";
-    lapis.fillText(`Colisão: ${detectColision()}`, quadro.width / 2, quadro.height);
+  // Limpando a tela para novo desenho
+  lapis.clearRect(0, 0, quadro.width, quadro.height);
+  // Desenhando o player1
+  lapis.beginPath();
+  lapis.strokeStyle = player1.cor;
+  lapis.arc(player1.posicaoX, player1.posicaoY, player1.size, 0, 2 * Math.PI);
+  lapis.fillRect(player1.posicaoX, player1.posicaoY, 1, 1);
+  lapis.stroke();
+  // Desenhando o player2
+  lapis.beginPath();
+  lapis.strokeStyle = player2.cor;
+  lapis.rect(player2.posicaoX - (TAMANHO / 2), player2.posicaoY - (TAMANHO / 2), player2.size, player2.size);
+  lapis.fillRect(player2.posicaoX, player2.posicaoY, 1, 1);
+  lapis.stroke();
+  // Desenhando o debug de posicionamentos
+  lapis.fillStyle = "black";
+  lapis.beginPath();
+  lapis.moveTo(player2.posicaoX, player2.posicaoY);
+  lapis.lineTo(player1.posicaoX, player1.posicaoY);
+  lapis.stroke();
+  lapis.font = "12px Arial";
+  lapis.textAlign = "center";
+  lapis.fillText(`Distância ${Math.sqrt(Math.pow((player2.posicaoX - player1.posicaoX), 2) + Math.pow((player2.posicaoY - player1.posicaoY), 2)).toFixed(2)}`, quadro.width / 2, quadro.height - 12)
+  lapis.fillText(`Colisão: ${(detectColision()) ? "True" : "False"}`, quadro.width / 2, quadro.height);
 }
